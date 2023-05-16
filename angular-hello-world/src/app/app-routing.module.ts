5d984067426e243ed344206b677b9d2d5f435fb2
thomaseade <eadethomas95@gmail.com>
2023-05-16 16:11:16 +0200

    routing

--- angular-hello-world/src/app/app-routing.module.ts
+++ angular-hello-world/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { registerLocaleData } from '@angular/common';
+import { RegisterComponent } from './pages/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'register', component:RegisterComponent},
+  {path:'home', component:HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
