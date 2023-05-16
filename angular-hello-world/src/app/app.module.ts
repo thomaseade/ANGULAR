b1ec812e40063e3e7149a914785533a1c11b83a9
thomaseade <eadethomas95@gmail.com>
2023-05-16 15:33:50 +0200

    pagina register

--- angular-hello-world/src/app/app.module.ts
+++ angular-hello-world/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './component/header/header.component';
 import { FooterComponent } from './component/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { FormRegisterComponent } from './component/form-register/form-register.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 
 @NgModule({
@@ -16,6 +17,7 @@ import { FormRegisterComponent } from './component/form-register/form-register.c
     FooterComponent,
     HomeComponent,
     FormRegisterComponent,
+    RegisterComponent,
   
   ],
   imports: [
