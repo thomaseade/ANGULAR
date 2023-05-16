5073aa4f6c3d7af6b2af1214c41b766b927cc34c
thomaseade <eadethomas95@gmail.com>
2023-05-16 14:33:06 +0200

    reto4 footer

--- angular-hello-world/src/app/app.module.ts
+++ angular-hello-world/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    FooterComponent,
   
   ],
   imports: [
